5334c0f4831907a622be0487dfa1df5045831337
Brianna Honeywell <wellhoneyb@Briannas-MacBook-Pro.local>
2021-03-27 17:59:11 -0400

    removing initial startup file

--- test.py
+++
@@ -1,9 +0,0 @@
-import spotipy
-import pandas as pd
-from spotipy.oauth2 import SpotifyClientCredentials
-
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager
-=
-client_credentials_manager)
-
